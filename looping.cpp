7a7dfc120dcb55ca7f1d2178e01ffe843307b5f7
SyahJehanWonopatih <syahjehan00@gmail.com>
2025-03-17 15:46:37 +0700

    menambahkan perulangan for pencacah naik

--- looping.cpp
+++ looping.cpp
@@ -7,7 +7,15 @@ int main()
     int n;
     int perulanganWhile = 0;
     int perulanganDo = 0;
-     
+       // perulangan dengan for
+       cout << "perulangan for pencacah naik" << endl;
+       for (n = 0; n < 5; n++) 
+       {
+           cout << "nilai n = " << n << 
+       }
+       cout << "nilai n terakhir = " << n << endl;
+       cout << endl;
+   
 }
 
 
