50b3da84f99912ef0bd81c0128d6289af25a2ec2
SyahJehanWonopatih <syahjehan00@gmail.com>
2025-03-17 15:51:50 +0700

    menambahkan perulangan for pencacah turun

--- looping.cpp
+++ looping.cpp
@@ -1,4 +1,4 @@
-#include <iostrem>
+#include <iostream>
 #include <ctime>
 #include <cstdlib>
 using namespace std;
@@ -11,11 +11,19 @@ int main()
        cout << "perulangan for pencacah naik" << endl;
        for (n = 0; n < 5; n++) 
        {
-           cout << "nilai n = " << n << 
+           cout << "nilai n = " << n << "selamat datang" << endl;
        }
        cout << "nilai n terakhir = " << n << endl;
        cout << endl;
    
+       cout << "perulangan for pencacah turun" << endl;
+       //perulangan dengan for
+       for (n = 5; n > 0; n--) 
+       {
+           cout << "nilai n = " << n << "selamat datang" << endl;
+       }
+       cout << "nilai n terakhir = " << n;
+       cout << endl;
 }
 
 
